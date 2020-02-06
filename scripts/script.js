62484d74971d98284ced2ae3efecd1a2fdf74847
Matthew Hernandez <matt.hernandez94@gmail.com>
2020-02-06 17:17:22 -0500

    initial commit

---
+++ scripts/script.js
@@ -0,0 +1,13 @@
+console.log("suh dude");
+
+// pseudo code
+// variable declaration 
+// 
+
+// event listener function (click)
+// play mp3 file
+
+// darkmode event listener function (click)
+// change background color
+// change text color
+// change button color
