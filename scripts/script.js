e9a1dbea3ad0576a4c49f1a10094ceb947d45d0e
Matthew Hernandez <matt.hernandez94@gmail.com>
2020-02-07 16:10:46 -0500

    selected 16 samples

--- scripts/script.js
+++ scripts/script.js
@@ -2,7 +2,11 @@ console.log("suh dude");
 
 // pseudo code
 // variable declaration 
-// 
+var audio = new Audio("./Samples/oneShots/LCU_808_01_C.wav");
+
+$("h1").on("click", function() {
+    audio.play();
+});
 
 // event listener function (click)
 // play mp3 file
