df3d440bed2d2e59e31fdcbb342d0bfba0ef0844
Matthew Hernandez <matt.hernandez94@gmail.com>
2020-02-10 10:36:38 -0500

    adjusted markup structure, and js object structure

--- scripts/script.js
+++ scripts/script.js
@@ -1,17 +1,44 @@
 console.log("suh dude");
 
-// pseudo code
-// variable declaration 
-var audio = new Audio("./Samples/oneShots/LCU_808_01_C.wav");
 
-$("h1").on("click", function() {
-    audio.play();
+var samples = {
+    // first row variables
+    clapOne: new Audio("./samples/clapOne.wav"),
+    clapTwo: new Audio("./samples/clapTwo.wav"),
+    triangle: new Audio("./samples/triangle.wav"),
+    voxCymbal: new Audio("./samples/voxCymbal.wav"),
+
+    // second row variables
+    snareOne: new Audio("./samples/snareOne.wav"),
+    snareTwo: new Audio("./samples/snareTwo.wav"),
+    snareThree: new Audio("./samples/snareThree.wav"),
+    snareFour: new Audio("./samples/snareFour.wav"),
+
+    // third row variables
+    kickOne: new Audio("./samples/kickOne.wav"),
+    kickTwo: new Audio("./samples/kickTwo.wav"),
+    kickThree: new Audio("./samples/kickThree.wav"),
+    kickFour: new Audio("./samples/kickFour.wav"),
+
+    // fourth row variables
+    bassOne: new Audio("./samples/bassOne.wav"),
+    bassTwo: new Audio("./samples/bassTwo.wav"),
+    bassThree: new Audio("./samples/bassThree.wav"),
+    bassFour: new Audio("./samples/bassFour.wav"),
+}
+
+$(".drumBeat").on("click", function () {
+    samples.snareOne.play();
 });
 
-// event listener function (click)
-// play mp3 file
+document.addEventListener("keydown", function(e) {
+    let keyCode = e.keyCode;
+    let keyCodeString = keyCode.toString();
+    console.log(keyCode);
+    samples.keyCodeString.play();
 
-// darkmode event listener function (click)
-// change background color
-// change text color
-// change button color
+    // let audio = document.querySelector("audio[id='" + keyCode + "']");
+    // audio.play();
+    // audio.currentTime = 0;
+})
+
