443f4cccbc2da8251530f41c7c6685e57ba69fc1
Matthew Hernandez <matt.hernandez94@gmail.com>
2020-04-13 21:15:00 -0400

    added anime js and animations

--- scripts/script.js
+++ scripts/script.js
@@ -11,55 +11,55 @@
 // namespace
 const samples = {
     
-    // first row variables
-    "49": new Audio("./samples/clapOne.wav"),
-    "50": new Audio("./samples/clapTwo.wav"),
-    "51": new Audio("./samples/airhorn.wav"),
-    "52": new Audio("./samples/inception.mp3"),
+	// first row variables
+	"49": new Audio("./samples/clapOne.wav"),
+	"50": new Audio("./samples/clapTwo.wav"),
+	"51": new Audio("./samples/airhorn.wav"),
+	"52": new Audio("./samples/inception.mp3"),
 
-    // second row variables
-    "81": new Audio("./samples/hatClosedOne.wav"),
-    "87": new Audio("./samples/hatClosedTwo.wav"),
-    "69": new Audio("./samples/hatOpenOne.wav"),
-    "82": new Audio("./samples/hatOpenTwo.wav"),
+	// second row variables
+	"81": new Audio("./samples/hatClosedOne.wav"),
+	"87": new Audio("./samples/hatClosedTwo.wav"),
+	"69": new Audio("./samples/hatOpenOne.wav"),
+	"82": new Audio("./samples/hatOpenTwo.wav"),
 
-    // third row variables
-    "65": new Audio("./samples/snareOne.wav"),
-    "83": new Audio("./samples/snareTwo.wav"),
-    "68": new Audio("./samples/snareThree.wav"),
-    "70": new Audio("./samples/snareFour.wav"),
-    
-    // fourth row variables
-    "90": new Audio("./samples/kickOne.wav"),
-    "88": new Audio("./samples/kickTwo.wav"),
-    "67": new Audio("./samples/kickThree.wav"),
-    "86": new Audio("./samples/kickFour.wav"),
+	// third row variables
+	"65": new Audio("./samples/snareOne.wav"),
+	"83": new Audio("./samples/snareTwo.wav"),
+	"68": new Audio("./samples/snareThree.wav"),
+	"70": new Audio("./samples/snareFour.wav"),
+	
+	// fourth row variables
+	"90": new Audio("./samples/kickOne.wav"),
+	"88": new Audio("./samples/kickTwo.wav"),
+	"67": new Audio("./samples/kickThree.wav"),
+	"86": new Audio("./samples/kickFour.wav"),
 
-    // color randomizer
-    "1": "#F7EC4F", // yellow
-    "2": "#F79665", // orange
-    "3": "#63FFE5", // blue
-    "4": "#24FF9E", // green
+	// color randomizer
+	"1": "#F7EC4F", // yellow
+	"2": "#F79665", // orange
+	"3": "#63FFE5", // blue
+	"4": "#24FF9E", // green
 
-    // visualizer array
-    // rng selects random className within array between c - h
-    // rng +/- will select surrounding divs to increase height to add the "wave" look
-    bar: ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j"],
+	// visualizer array
+	// rng selects random className within array between c - h
+	// rng +/- will select surrounding divs to increase height to add the "wave" look
+	bar: [".a", ".b", ".c", ".d", ".e", ".f", ".g", ".h", ".i", ".j"],
 }
 
 // visualizer fn
-samples.visualizer = () => {
-    $(".drumBeat").mousedown(function() {
-        const random = Math.floor(Math.random() * 6) + 2;
-        const peak = samples.bar[random];
-        const minusOne = samples.bar[random - 1];
-        const minusTwo = samples.bar[random - 2];
-        const plusOne = samples.bar[random + 1];
-        const plusTwo = samples.bar[random + 2];
-        
-        
-    });
-}
+// samples.visualizer = () => {
+// 	$(".drumBeat").mousedown(function() {
+// 		const random = Math.floor(Math.random() * 6) + 2;
+// 		const peak = samples.bar[random];
+// 		const minusOne = samples.bar[random - 1];
+// 		const minusTwo = samples.bar[random - 2];
+// 		const plusOne = samples.bar[random + 1];
+// 		const plusTwo = samples.bar[random + 2];
+		
+// 		samples.animate(peak, minusOne, minusTwo, plusOne, plusTwo);
+// 	});
+// }
 
 
 // click event method (MVP)
@@ -67,19 +67,27 @@ samples.visualizer = () => {
 // .slice(9, 11) removes first class name and whitespace
 // colorValue returns a random color from the sample object 
 samples.clickEvent = function() {
-    $(".drumBeat").mousedown(function() {
-        const classes = this.className.slice(9, 11);
-        samples[classes].currentTime = 0;
-        samples[classes].play();
-        colorValue = Math.floor(Math.random() * 4) + 1;
+	$(".drumBeat").mousedown(function() {
+		const classes = this.className.slice(9, 11);
+		samples[classes].currentTime = 0;
+		samples[classes].play();
+		// colorValue = Math.floor(Math.random() * 4) + 1;
 
-        $(`.${classes}`).css("background-color", `${samples[colorValue]}`);
+		const random = Math.floor(Math.random() * 6) + 2;
+		const peak = samples.bar[random];
+		const minusOne = samples.bar[random - 1];
+		const minusTwo = samples.bar[random - 2];
+		const plusOne = samples.bar[random + 1];
+		const plusTwo = samples.bar[random + 2];
 
-    }).mouseup(function(){
-        const classes = this.className.slice(9, 11);
-        $(`.${classes}`).css("background-color", "#D0D0D1");
+		samples.animate(peak, minusOne, minusTwo, plusOne, plusTwo);
+		$(`.${classes}`).css("background-color", "#d8d8d8");
 
-    });
+	}).mouseup(function(){
+		const classes = this.className.slice(9, 11);
+		$(`.${classes}`).css("background-color", "#ffffff");
+
+	});
 },
 
 
@@ -89,30 +97,72 @@ samples.clickEvent = function() {
 // .toString() converts the keyCode from number to string, as numbers can't be used as object keys
 // colorValue returns a random color from the sample object 
 samples.keyboardEvent = () => {
-    $(this).keydown(function(event) {
-        const key = event.keyCode.toString();
-        samples[key].currentTime = 0;
-        samples[key].play();
-        colorValue = Math.floor(Math.random() * 4) + 1;
+	$(this).keydown(function(event) {
+		const key = event.keyCode.toString();
+		samples[key].currentTime = 0;
+		samples[key].play();
+		colorValue = Math.floor(Math.random() * 4) + 1;
 
-        $(`.${key}`).css("background-color", `${samples[colorValue]}`);
+		$(`.${key}`).css("background-color", `${samples[colorValue]}`);
 
-    }).keyup(function(event) {
-        const key = event.keyCode.toString();
-        $(`.${key}`).css("background-color", "#D0D0D1");
-        
-    });
+	}).keyup(function(event) {
+		const key = event.keyCode.toString();
+		$(`.${key}`).css("background-color", "#D0D0D1");
+			
+	});
 },
 
+samples.animate = function(one, two, three, four, five) {
+
+	anime({
+		targets: ".visualizerBar",
+		height: ["0%", "5%"],
+		easing: "easeInOutQuad",
+		direction: 'alternate',
+		duration: 165,
+	})
+
+	anime({
+		targets: one,
+		height: ["0%", "90%"],
+		easing: "easeInOutQuad",
+		direction: 'alternate',
+		duration: 150,
+	});
+
+	anime({
+		targets: two,
+		height: ["0%", "60%"],
+		easing: "easeInOutQuad",
+		direction: 'alternate',
+		duration: 155,
+	})
+
+	anime({
+		targets: [three, four],
+		height: ["0%", "30%"],
+		easing: "easeInOutQuad",
+		direction: 'alternate',
+		duration: 160,
+	})
+
+	anime({
+		targets: five,
+		height: ["0%", "15%"],
+		easing: "easeInOutQuad",
+		direction: 'alternate',
+		duration: 160,
+	})
+}
 
 /////////////////////////////////////
 
 
 // init fn
 samples.init = function() {
-    this.keyboardEvent();
-    this.clickEvent();
-    this.visualizer();
+	this.keyboardEvent();
+	this.clickEvent();
+	this.visualizer();
 };
 
 
@@ -121,6 +171,6 @@ samples.init = function() {
 
 // doc ready fn
 $(function() {
-    samples.init();
+	samples.init();
 });
 
