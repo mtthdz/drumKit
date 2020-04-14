0142eb57a728735973baf9720f97b549c287deb6
Matthew Hernandez <matt.hernandez94@gmail.com>
2020-04-14 12:27:29 -0400

    deleted unneeded samples, refactored js

--- scripts/script.js
+++ scripts/script.js
@@ -10,30 +10,12 @@
 
 // namespace
 const samples = {
-    
-	// first row variables
-	"49": new Audio("./samples/clapOne.wav"),
-	"50": new Audio("./samples/clapTwo.wav"),
-	"51": new Audio("./samples/airhorn.wav"),
-	"52": new Audio("./samples/inception.mp3"),
+	"81": new Audio("./samples/kick.wav"),
+	"87": new Audio("./samples/snare.wav"),
+	"69": new Audio("./samples/hatClosed.wav"),
+	"82": new Audio("./samples/hatOpen.wav"),
+	"84": new Audio("./samples/clap.wav"),
 
-	// second row variables
-	"81": new Audio("./samples/hatClosedOne.wav"),
-	"87": new Audio("./samples/hatClosedTwo.wav"),
-	"69": new Audio("./samples/hatOpenOne.wav"),
-	"82": new Audio("./samples/hatOpenTwo.wav"),
-
-	// third row variables
-	"65": new Audio("./samples/snareOne.wav"),
-	"83": new Audio("./samples/snareTwo.wav"),
-	"68": new Audio("./samples/snareThree.wav"),
-	"70": new Audio("./samples/snareFour.wav"),
-	
-	// fourth row variables
-	"90": new Audio("./samples/kickOne.wav"),
-	"88": new Audio("./samples/kickTwo.wav"),
-	"67": new Audio("./samples/kickThree.wav"),
-	"86": new Audio("./samples/kickFour.wav"),
 
 	// color randomizer
 	"1": "#F7EC4F", // yellow
@@ -71,16 +53,7 @@ samples.clickEvent = function() {
 		const classes = this.className.slice(9, 11);
 		samples[classes].currentTime = 0;
 		samples[classes].play();
-		// colorValue = Math.floor(Math.random() * 4) + 1;
-
-		const random = Math.floor(Math.random() * 6) + 2;
-		const peak = samples.bar[random];
-		const minusOne = samples.bar[random - 1];
-		const minusTwo = samples.bar[random - 2];
-		const plusOne = samples.bar[random + 1];
-		const plusTwo = samples.bar[random + 2];
-
-		samples.animate(peak, minusOne, minusTwo, plusOne, plusTwo);
+		samples.animate();
 		$(`.${classes}`).css("background-color", "#d8d8d8");
 
 	}).mouseup(function(){
@@ -101,18 +74,24 @@ samples.keyboardEvent = () => {
 		const key = event.keyCode.toString();
 		samples[key].currentTime = 0;
 		samples[key].play();
-		colorValue = Math.floor(Math.random() * 4) + 1;
-
-		$(`.${key}`).css("background-color", `${samples[colorValue]}`);
+		samples.animate();		
+		$(`.${key}`).css("background-color", "#d8d8d8");
 
 	}).keyup(function(event) {
 		const key = event.keyCode.toString();
-		$(`.${key}`).css("background-color", "#D0D0D1");
+		$(`.${key}`).css("background-color", "#ffffff");
 			
 	});
 },
 
-samples.animate = function(one, two, three, four, five) {
+
+samples.animate = function() {
+	const random = Math.floor(Math.random() * 6) + 2;
+	const peak = samples.bar[random];
+	const minusOne = samples.bar[random - 1];
+	const minusTwo = samples.bar[random - 2];
+	const plusOne = samples.bar[random + 1];
+	const plusTwo = samples.bar[random + 2];
 
 	anime({
 		targets: ".visualizerBar",
@@ -123,7 +102,7 @@ samples.animate = function(one, two, three, four, five) {
 	})
 
 	anime({
-		targets: one,
+		targets: peak,
 		height: ["0%", "90%"],
 		easing: "easeInOutQuad",
 		direction: 'alternate',
@@ -131,7 +110,7 @@ samples.animate = function(one, two, three, four, five) {
 	});
 
 	anime({
-		targets: two,
+		targets: minusOne,
 		height: ["0%", "60%"],
 		easing: "easeInOutQuad",
 		direction: 'alternate',
@@ -139,7 +118,7 @@ samples.animate = function(one, two, three, four, five) {
 	})
 
 	anime({
-		targets: [three, four],
+		targets: [minusTwo, plusOne],
 		height: ["0%", "30%"],
 		easing: "easeInOutQuad",
 		direction: 'alternate',
@@ -147,7 +126,7 @@ samples.animate = function(one, two, three, four, five) {
 	})
 
 	anime({
-		targets: five,
+		targets: plusTwo,
 		height: ["0%", "15%"],
 		easing: "easeInOutQuad",
 		direction: 'alternate',
@@ -162,7 +141,6 @@ samples.animate = function(one, two, three, four, five) {
 samples.init = function() {
 	this.keyboardEvent();
 	this.clickEvent();
-	this.visualizer();
 };
 
 
